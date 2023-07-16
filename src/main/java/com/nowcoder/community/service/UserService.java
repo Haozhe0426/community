f867a26a5d45a5573172cce874c8debceefa2873
Haozhe0426 <2573058590@qq.com>
2023-07-16 10:48:02 +0800

    登录功能

--- src/main/java/com/nowcoder/community/service/UserService.java
+++ src/main/java/com/nowcoder/community/service/UserService.java
@@ -152,7 +152,9 @@ public class UserService implements CommunityConstant {
         loginTicketMapper.updateStatus(ticket, 1);
     }
 
-
+    public LoginTicket findLoginTicket(String ticket){
+        return loginTicketMapper.selectByTicket(ticket);
+    }
 
 
 
