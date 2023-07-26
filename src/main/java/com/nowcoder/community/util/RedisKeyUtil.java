da70d5ff695f4033a245ce9d070334dd3f80c383
Haozhe0426 <2573058590@qq.com>
2023-07-26 17:07:28 +0800

    点赞

---
+++ src/main/java/com/nowcoder/community/util/RedisKeyUtil.java
@@ -0,0 +1,14 @@
+package com.nowcoder.community.util;
+
+public class RedisKeyUtil {
+
+    private static final String SPLIT = ":";
+    private static final String PREFIX_ENTITY_LIKE = "like:entity";
+
+    // 某个实体的赞
+    // 键：like:entity:entityType:entityId  值：set(userId)
+    public static String getEntityLikeKey(int entityType, int entityId) {
+        return PREFIX_ENTITY_LIKE + SPLIT + entityType + SPLIT + entityId;
+    }
+
+}
